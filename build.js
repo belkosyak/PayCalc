fe55213b9592b2910d82eab038e48711ae37ca48
Constantin Belenkiy <belkosyak@gmail.com>
2015-02-23 12:36:25 +0600

    add oprimized files

---
+++ build.js
@@ -0,0 +1,10 @@
+({
+    baseUrl: 'js',
+    name: 'main',
+    out: 'build/built.js',
+    mainConfigFile: 'js/main.js',
+    paths: {
+        requireLib: '../bower_components/requirejs/require'
+    },
+    include: 'requireLib'
+})
