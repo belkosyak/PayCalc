9ccd2f764f56ff2810ea1e457f23f833f3b4645e
belkosyak <belkosyak@gmail.com>
2015-05-19 16:44:56 +0600

    make checkboxes more easy to check

--- js/views/pages/AddPaymentPageView.js
+++ js/views/pages/AddPaymentPageView.js
@@ -25,12 +25,10 @@ function (_, Backbone, PageView, Payment, Partner, PaymentSubject) {
             </div>\n\
             <div class="form-group">\n\
               <div class="col-xs-offset-3 col-xs-9">\n\
-                <div class="checkbox">\n\
-                  <label>\n\
-                    <input type="checkbox" id="is_loan" />\n\
-                    Is Loan</label>\n\
-                  </label>\n\
-                </div>\n\
+                <label class="checkbox-inline">\n\
+                  <input type="checkbox" id="is_loan" />\n\
+                  Is Loan</label>\n\
+                </label>\n\
               </div>\n\
             </div>\n\
           </div>\n\
@@ -39,7 +37,11 @@ function (_, Backbone, PageView, Payment, Partner, PaymentSubject) {
             <div class="form-group">\n\
               <div id="select_all_partners"\n\
                   class="btn btn btn-success btn-sm">\n\
-                Select all partners\n\
+                Select all\n\
+              </div>\n\
+              <div id="deselect_all_partners"\n\
+                  class="btn btn btn-success btn-sm">\n\
+                Deselect all\n\
               </div>\n\
             </div>\n\
             <div class="payment-partner-list">\n\
@@ -64,14 +66,17 @@ function (_, Backbone, PageView, Payment, Partner, PaymentSubject) {
 
     events: {
       'click .add-payment-button':  'addPayment',
-      'click #select_all_partners': 'checkAllPartners'
+      'click #select_all_partners': 'checkAllPartners',
+      'click #deselect_all_partners': 'uncheckAllPartners',
+      'change #is_loan': 'toggleIsLoan'
     },
 
     render: function () {
-      var partners = this.options.event.get('partners');
+      var partners = this.options.event.get('partners').clone();
+      partners = this.makePayerFirst(partners, this.options.payer);
       this.$el.html(this.template({
         eventId: this.options.eventId,
-        partners: this.options.event.get('partners'),
+        partners: partners,
         backPath: this.options.backPath,
         payer: this.options.payer
       }));
@@ -83,15 +88,17 @@ function (_, Backbone, PageView, Payment, Partner, PaymentSubject) {
       var event = this.options.event;
       var partners = event.get('partners');
       var errors;
+
+      var name = this.$('#name').val();
+      var cost = parseFloat(this.$('#cost').val());
+      var isLoan = this.$('#is_loan').prop('checked');
+
       // Collect all checked partners.
       this.$('input:checkbox').each(function () {
         if ($(this).prop('checked')) {
           paymentPartners.push(partners.get($(this).data('id')));
         }
       });
-      var name = this.$('#name').val();
-      var cost = parseFloat(this.$('#cost').val());
-      var isLoan = this.$('#is_loan').prop('checked');
 
       if (isLoan && paymentPartners.length > 2) {
         errors = ['You can lend only one person.'];
@@ -125,7 +132,17 @@ function (_, Backbone, PageView, Payment, Partner, PaymentSubject) {
     },
 
     checkAllPartners: function () {
-      this.$el.find('.payment-partner-list input:checkbox').prop('checked', true);
+      this.$('.payment-partner-list input:checkbox').prop('checked', true);
+    },
+
+    uncheckAllPartners: function () {
+      this.$('.payment-partner-list input:checkbox:not(:disabled)')
+        .prop('checked', false);
+    },
+
+    toggleIsLoan: function () {
+      var isLoan = this.$('#is_loan').prop('checked');
+      debugger;
     },
 
     generateName: function (name, cost, isLoan, paymentPartners) {
@@ -149,6 +166,12 @@ function (_, Backbone, PageView, Payment, Partner, PaymentSubject) {
         generatedName = cost;
       }
       return generatedName;
+    },
+
+    makePayerFirst: function (partners, payer) {
+      payer = partners.remove(payer);
+      partners.unshift(payer);
+      return partners;
     }
 
   });
